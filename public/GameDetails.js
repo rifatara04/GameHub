3e8910b78e992dcd68352fbca85e2a092d05b1aa
rifatara04 <rifatara42043@gmail.com>
2025-10-25 20:19:36 +0600

    json

--- public/GameDetails.js
+++ public/GameDetails.js
@@ -1,76 +1,6 @@
 [
-  {
+    {
     "id": "1",
-    "title": "Player Unknown's Battleground: PUBG",
-    "coverPhoto": "https://cdn1.epicgames.com/spt-assets/53ec4985296b4facbe3a8d8d019afba9/pubg-battlegrounds-19vwb.jpg",
-    "category": "FPS",
-    "downloadLink": "https://www.pubgmobile.com/en-US/home.shtml",
-    "description": "PUBG Mobile is a fast-paced battle royale game where players fight for survival, strategy, and victory on dynamic maps.",
-    "ratings": "4.5",
-    "developer": "Krafton"
-  },
-  {
-    "id": "2",
-    "title": "Call of Duty: Mobile",
-    "coverPhoto": "https://cdn.prod.website-files.com/65956e2711516206d2d1258f/6634c7715af10f24b00dc7b1_CODM%202663x1384.webp",
-    "category": "FPS",
-    "downloadLink": "https://www.callofduty.com/mobile",
-    "description": "COD Mobile delivers classic Call of Duty multiplayer and battle royale gameplay optimized for mobile devices.",
-    "ratings": "4.6",
-    "developer": "TiMi Studios"
-  },
-  {
-    "id": "3",
-    "title": "Valorant",
-    "coverPhoto": "https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-4f0cdb8f0cb0d06ea289ed24b28819b7",
-    "category": "FPS",
-    "downloadLink": "https://playvalorant.com/",
-    "description": "Valorant is a tactical FPS where precise shooting and team strategy define victory.",
-    "ratings": "4.7",
-    "developer": "Riot Games"
-  },
-  {
-    "id": "4",
-    "title": "Fortnite",
-    "coverPhoto": "https://dropinblog.net/34253310/files/featured/imagem-2024-09-26-103919931.png",
-    "category": "FPS",
-    "downloadLink": "https://www.epicgames.com/fortnite/en-US/home",
-    "description": "Fortnite is a battle royale game combining shooting, building mechanics, and fun events.",
-    "ratings": "4.4",
-    "developer": "Epic Games"
-  },
-  {
-    "id": "5",
-    "title": "Apex Legends",
-    "coverPhoto": "https://images.ctfassets.net/w5r1fvmogo3f/5fJySxdWKgRuLfRtdn81O4/4d7c1bf0af3075638a36926bfc1b32ad/3b4965a458fb41958c17ebf4d79475f0.webp",
-    "category": "FPS",
-    "downloadLink": "https://www.ea.com/games/apex-legends",
-    "description": "Apex Legends is a squad-based battle royale with unique characters and abilities.",
-    "ratings": "4.5",
-    "developer": "Respawn Entertainment"
-  },
-  {
-    "id": "6",
-    "title": "The Witcher 3: Wild Hunt",
-    "coverPhoto": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/292030/ss_608af6cfe0eab3f37265550b391732a3e88d1a4f.1920x1080.jpg?t=1761131270",
-    "category": "RPG",
-    "downloadLink": "https://thewitcher.com/en/witcher3",
-    "description": "An epic RPG set in a fantasy world where players hunt monsters and make impactful choices.",
-    "ratings": "4.9",
-    "developer": "CD Projekt Red"
-  },
-  {
-    "id": "7",
-    "title": "Cyberpunk 2077",
-    "coverPhoto": "https://static.cdprojektred.com/cms.cdprojektred.com/16x9_big/12aaa3b137a18e180bb92682e8f81674dcb7451f-1280x720.jpg",
-    "category": "RPG",
-    "downloadLink": "https://www.cyberpunk.net/",
-    "description": "Cyberpunk 2077 is an open-world RPG set in Night City, where choices affect story outcomes.",
-    "ratings": "4.2",
-    "developer": "CD Projekt Red"
-  },
-  {
-    "id": "8",
     "title": "Elden Ring",
     "coverPhoto": "https://wallpapercave.com/wp/wp14551887.jpg",
     "category": "RPG",
@@ -80,7 +10,7 @@
     "developer": "FromSoftware"
   },
   {
-    "id": "9",
+    "id": "2",
     "title": "Skyrim",
     "coverPhoto": "https://assets.nintendo.com/image/upload/q_auto/f_auto/ncom/software/switch/70010000001369/3a41386d4b0999365727a21cc5c13853cfc244abca39b689bb79a339601e48c3",
     "category": "RPG",
@@ -90,7 +20,7 @@
     "developer": "Bethesda Game Studios"
   },
   {
-    "id": "10",
+    "id": "3",
     "title": "Dragon Age: Inquisition",
     "coverPhoto": "https://gaming-cdn.com/images/products/399/orig/dragon-age-inquisition-pc-game-origin-cover.jpg?v=1651137991",
     "category": "RPG",
@@ -100,7 +30,7 @@
     "developer": "BioWare"
   },
   {
-    "id": "11",
+    "id": "4",
     "title": "World of Warcraft",
     "coverPhoto": "https://roundtablecoop.com/wp-content/uploads/2024/09/thewarwithintitle.jpg",
     "category": "MMORPG",
@@ -110,7 +40,7 @@
     "developer": "Blizzard Entertainment"
   },
   {
-    "id": "12",
+    "id": "5",
     "title": "Final Fantasy XIV",
     "coverPhoto": "https://xboxwire.thesourcemediaassets.com/sites/2/2024/03/FFXIV_XBX_FR_Promo-hero-a153579a7a92c06e9e2d-1024x576.jpg",
     "category": "MMORPG",
@@ -120,7 +50,7 @@
     "developer": "Square Enix"
   },
   {
-    "id": "13",
+    "id": "6",
     "title": "Guild Wars 2",
     "coverPhoto": "https://gaming-cdn.com/images/products/29/orig/guild-wars-2-pc-game-ncsoft-cover.jpg?v=1701274016",
     "category": "MMORPG",
@@ -130,7 +60,7 @@
     "developer": "ArenaNet"
   },
   {
-    "id": "14",
+    "id": "7",
     "title": "Black Desert Online",
     "coverPhoto": "https://gaming-cdn.com/images/products/676/orig/black-desert-online-pc-game-cover.jpg?v=1706020901",
     "category": "MMORPG",
@@ -140,7 +70,7 @@
     "developer": "Pearl Abyss"
   },
   {
-    "id": "15",
+    "id": "8",
     "title": "Lost Ark",
     "coverPhoto": "https://wallpapers.com/images/hd/lost-ark-game-pfgruf3t8ttef9dv.jpg",
     "category": "MMORPG",
@@ -148,5 +78,45 @@
     "description": "Lost Ark is an action-packed MMORPG with deep combat mechanics, quests, and dungeons.",
     "ratings": "4.6",
     "developer": "Smilegate RPG"
+  },
+  {
+    "id": "9",
+    "title": "Player Unknown's Battleground: PUBG",
+    "coverPhoto": "https://cdn1.epicgames.com/spt-assets/53ec4985296b4facbe3a8d8d019afba9/pubg-battlegrounds-19vwb.jpg",
+    "category": "FPS",
+    "downloadLink": "https://www.pubgmobile.com/en-US/home.shtml",
+    "description": "PUBG Mobile is a fast-paced battle royale game where players fight for survival, strategy, and victory on dynamic maps.",
+    "ratings": "4.5",
+    "developer": "Krafton"
+  },
+  {
+    "id": "10",
+    "title": "Call of Duty: Mobile",
+    "coverPhoto": "https://cdn.prod.website-files.com/65956e2711516206d2d1258f/6634c7715af10f24b00dc7b1_CODM%202663x1384.webp",
+    "category": "FPS",
+    "downloadLink": "https://www.callofduty.com/mobile",
+    "description": "COD Mobile delivers classic Call of Duty multiplayer and battle royale gameplay optimized for mobile devices.",
+    "ratings": "4.6",
+    "developer": "TiMi Studios"
+  },
+  {
+    "id": "11",
+    "title": "Valorant",
+    "coverPhoto": "https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-4f0cdb8f0cb0d06ea289ed24b28819b7",
+    "category": "FPS",
+    "downloadLink": "https://playvalorant.com/",
+    "description": "Valorant is a tactical FPS where precise shooting and team strategy define victory.",
+    "ratings": "4.7",
+    "developer": "Riot Games"
+  },
+  {
+    "id": "12",
+    "title": "Fortnite",
+    "coverPhoto": "https://dropinblog.net/34253310/files/featured/imagem-2024-09-26-103919931.png",
+    "category": "FPS",
+    "downloadLink": "https://www.epicgames.com/fortnite/en-US/home",
+    "description": "Fortnite is a battle royale game combining shooting, building mechanics, and fun events.",
+    "ratings": "4.4",
+    "developer": "Epic Games"
   }
 ]
